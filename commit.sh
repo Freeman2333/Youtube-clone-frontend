17840f6543db9ee9e1ca1926e1b069d838eeffac
Webdozen <microlion116@gmail.com>
2020-05-05 10:22:34 +0300

    finished homepage

---
+++ commit.sh
@@ -0,0 +1,12 @@
+Y=2020
+M=5
+D=5
+i=22
+comment="finished homepage "
+
+export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
+export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
+# git add commit.md -f
+git add .
+git commit --date="$Y-$M-$D 10:$i:34" -m "$comment"
+git push
