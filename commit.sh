2e1c0df8d4a23d9342dc0e964b91475bab429e77
Webdozen <microlion116@gmail.com>
2020-05-05 10:22:34 +0300

    added Channel

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=5
 i=22
-comment="added Trending page, Subscriptions page"
+comment="added Channel"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
