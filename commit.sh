420bc73e22c7eb4b60b24a93407631116d210bc1
Webdozen <microlion116@gmail.com>
2020-05-05 10:22:34 +0300

    added Trending page, Subscriptions page

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=5
 i=22
-comment="finished homepage "
+comment="added Trending page, Subscriptions page"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
