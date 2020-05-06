8bc950a6708a920aab3a6cd0ec5f96cf47cede77
Webdozen <microlion116@gmail.com>
2020-05-06 10:22:34 +0300

    redux and backend integration

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=6
 i=22
-comment="added likedvideos, watchlater, library, yourvideos"
+comment="redux and backend integration"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
