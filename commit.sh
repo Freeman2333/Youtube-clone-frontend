9d4d6e6b59967ebee04c35b23ba181155b93ab76
Webdozen <microlion116@gmail.com>
2020-05-07 10:22:34 +0300

    updated README, added search

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=7
 i=22
-comment="axios instance, env setup, like/dislike, subscribe/unsubscribe, responsive"
+comment="updated README, added search "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
