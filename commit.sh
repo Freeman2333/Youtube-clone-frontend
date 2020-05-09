63ccf2c72fa44f363fef1bd90d11230578f116ba
Webdozen <microlion116@gmail.com>
2020-05-09 10:22:34 +0300

    added History, NoResults

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=9
 i=22
-comment="isFetching for loader, search options, suggestions"
+comment="added History, NoResults"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
