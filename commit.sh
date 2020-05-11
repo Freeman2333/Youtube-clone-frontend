b4f53ac702ee0799b6328686233865619a300b14
Webdozen <microlion116@gmail.com>
2020-05-11 10:22:34 +0300

    moved to aws (backend)

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=11
 i=22
-comment="refresh on logout/login/signup"
+comment="moved to aws (backend)"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
