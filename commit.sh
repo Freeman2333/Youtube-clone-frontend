0b12280108bae59ae8e2f5bcd30f250c249782f0
Webdozen <microlion116@gmail.com>
2020-05-15 10:22:34 +0300

    index.html description

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 Y=2020
 M=5
-D=14
+D=15
 i=22
-comment="limit video size to 30mb"
+comment="index.html description"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
