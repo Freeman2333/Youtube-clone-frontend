fb42737968331c956fe7d105038ebd8d91a2a1fe
Webdozen <microlion116@gmail.com>
2020-05-17 10:22:34 +0300

    title trending card width

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=17
 i=22
-comment="ecaca1e title width fix"
+comment="title trending card width"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
