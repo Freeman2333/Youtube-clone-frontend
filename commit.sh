e9a1679bd7f43bcc28e46f645dad452e60ad997e
Webdozen <microlion116@gmail.com>
2020-05-20 10:22:34 +0300

    wrong status badge

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 Y=2020
 M=5
-D=19
+D=20
 i=22
-comment="code format prettier"
+comment="wrong status badge"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
